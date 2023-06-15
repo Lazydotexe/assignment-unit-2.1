ae0c973aeceea52e4b316dfd7f18ea677291941b
Drew Cannon <papaporo@proton.me>
2023-06-14 23:33:24 -0500

    Finished all 18 steps. I wrote #16 as a simple if/else statement but i still have trouble understanding functions so i decided to challenge myself to write one.

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -35,8 +35,7 @@ if (adventurous) {
 else {
     console.log("How about we stay home?");
 }
-// or
-adventurous ? console.log("Adventures are great!") : console.log("How about we stay home?");
+
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
@@ -50,7 +49,15 @@ if (luckyNumber == 2 && adventurous == true) {
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-
+if (pets < allowedPets) {
+    console.log("I can have more pets!");
+  }
+  else if (pets === allowedPets) {
+    console.log("I have enough pets");
+  }
+  else {
+    console.log("Oh no, I have too many pets!")
+  }
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
@@ -58,6 +65,21 @@ if (luckyNumber == 2 && adventurous == true) {
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
+function furbabies(pets, friendsPets) {
+    let mostPets;
+  
+    if (pets > friendsPets) {
+      mostPets = pets;
+    } else {
+      mostPets = friendsPets;
+    }
+  
+    return mostPets;
+  }
+  
+  let mostPets = furbabies(pets, friendsPets);
+  
+  console.log("I have the most pets with", mostPets + '!');
 
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
@@ -65,7 +87,18 @@ if (luckyNumber == 2 && adventurous == true) {
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+switch (luckyNumber) {
+    case 1: console.log("First is the worst");
+    break;
+    case 2: console.log("Second is the best");
+    break;
+    case 3: console.log("Third is the one with the polka dot dress");
+    break;
+    default: console.log("Luck is what happens when preparation meets opportunity")
+    break;
+}
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+adventurous ? console.log("Adventures are great!") : console.log("How about we stay home?");
 
 
